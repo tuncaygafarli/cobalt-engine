93c7383bca92cbce6c58a28c18a81c54b50a4415
Tuncay <68643463+tuncayofficial@users.noreply.github.com>
2025-12-21 23:18:46 +0400

    Fixed SoundManager issue

--- Managers/SoundManager.cpp
+++ Managers/SoundManager.cpp
@@ -6,6 +6,14 @@
 
 void SoundManager::playHitSound(const std::string& path)
 {
-	sf::Music sound(path);
+	sf::SoundBuffer buffer;
+
+	if (!buffer.loadFromFile(path)) 
+	{
+		std::cout << "Failed to load sound file." << std::endl;
+	}
+
+	sf::Sound sound(buffer);
+	sound.setVolume(100.f);
 	sound.play();
 }
